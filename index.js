2649635639eabbb2935a4f177bd1ee68c1a33a0d
davy_chen <davy_chen@epam.com>
2023-02-24 15:59:56 +0800

    Validation demo

---
+++ index.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const Joi = require('joi');
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./package');
+
+const init = async () => {
+
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+  });
+
+  const swaggerOptions = {
+    info: {
+      title: 'Test API Documentation',
+      version: Pack.version,
+    },
+  };
+
+  await server.register([
+    Inert,
+    Vision,
+    {
+      plugin: HapiSwagger,
+      options: swaggerOptions
+    }
+  ]);
+
+  server.route({
+    method: 'POST',
+    path: '/reset',
+    handler: (request, h) => {
+      return `${request.payload.username} to reset password`;
+    },
+    options: {
+      description: 'Get todo',
+      notes: 'Returns a todo item by the id passed in the path',
+      tags: ['api'],
+      validate: {
+        failAction: (request, h, err) => {
+          throw err;
+        },
+        payload: Joi.object({
+          username: Joi.string()
+            .alphanum()
+            .min(3)
+            .max(30)
+            .required().
+            messages({
+              "string.empty": "Username is not allowed to be empty",
+              "string.required": "Username is required lar",
+              "string.min": "Username must be at least 3 characters long",
+            }),
+          new_password: Joi.string().required()
+            .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+          confirm_password: Joi.ref('new_password'),
+          reset_token: Joi.string().required()
+        }).label('ResetPasswordForm')
+      }
+    }
+  });
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+  console.log(err);
+  process.exit(1);
+});
+
+init();
+
+
+
+
